bdb570991d1551b8b68be98e44a00a5387fd8be7
Hassan <hachehri@gmail.com>
2025-10-06 16:05:43 +0100

    add section swiper and creat footer and change some lines creat menu responsive navbar

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
 
 
+let menu=document.querySelector(".menu");
+let navBar=document.querySelector(".navbar");
+let header=document.querySelector("header");
+
+
+
+addEventListener("scroll",()=>{
+     header.classList.toggle('shadow',window.scrollY > 0 );
+});
+
+
+menu.addEventListener("click",()=>{
+   navBar.classList.toggle("Acitve-nav");
+
+
+})
+
+
+
+
 products_in_list=[
 
     {
@@ -99,3 +119,45 @@ function add(){
 }
 
 add();
+
+
+
+new Swiper(".wrapper", {
+  loop: true,
+  spaceBetween: 30,
+
+  // Autoplay
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+
+  // Pagination bullets
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // Responsive breakpoints
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+
